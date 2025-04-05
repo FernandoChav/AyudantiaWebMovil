400694b21733413a87a90afe6c9831a3633bdedc
Fernandin <fpatric25@gmail.com>
2025-04-05 18:02:42 -0300

    feat(model,data,repository,dto) se agregan las capas de abstraccion en data, se agrega nuevas entidades(esto no deberia estar aqui si no en otra branch XD) los repositorios correspondientes y el patron de diseño unit of work

---
+++ Ayudantia/src/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Ayudantia.Src.Models;
+
+namespace Ayudantia.Src.Interfaces;
+
+public interface IProductRepository
+{
+    Task<Product> GetProductByIdAsync(int id);
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task AddProductAsync(Product product);
+    void DeleteProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+}
